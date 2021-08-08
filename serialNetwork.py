c3dcfdabbd0fe20ddcf0c8982a20e590635ee9a1
qhdgkdbs <qhdgkdbs@gmail.com>
2021-08-08 18:00:30 +0900

    revise motor

--- serialNetwork.py
+++ serialNetwork.py
@@ -2,7 +2,7 @@ import serial
 import time
 import json
  
-msg = {'conveyor_step' : 0, 'sorting_step' : 0}
+msg = {'lPow' : 0, 'rPow' : 0}
 port = '/dev/ttyUSB0' # 시리얼 포트
 baud = 9600 # 시리얼 보드레이트(통신속도)
  
@@ -11,10 +11,10 @@ ser = serial.Serial(port,baud)
 end_str = '\n'
  
 while True:
-    print("conveyor_step_Input: ")
-    msg['conveyor_step'] = input() 
-    print("sorting_step_Input: ")
-    msg['sorting_step'] = input() 
+    # 모터 출력값 설정
+    msg['lPow'] = input() 
+    msg['rPow'] = input() 
+
     json_msg = json.dumps(msg)
     ser.write(json_msg.encode())
     ser.write(end_str.encode())
