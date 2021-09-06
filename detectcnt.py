71b59901b29273651bb432304471e7b13ecbf551
ahnsun98 <61009770+ahnsun98@users.noreply.github.com>
2021-09-06 21:45:42 +0900

    Update detectcnt.py

--- detectcnt.py
+++ detectcnt.py
@@ -1,4 +1,3 @@
-
 import jetson.inference
 import jetson.utils
 
@@ -7,56 +6,44 @@ import time
 import json
 
 
+# 혼잡도 계산을 위해 아두이노로 전송할 message (차 개수, 보이는 차의 면적, 사람 수, 보이는 사람의 면적)
+msg = {'car_cnt' : 0, 'car_area' : [], 'person_cnt' : 0, 'person_area' : []}
 
-# msg = {'car_cnt' : 0, 'car_area' : [], 'person_cnt' : 0, 'person_area' : []}
-pCount = 0
-
-ser = serial.Serial(
+ser = serial.Serial( # 시리얼 통신 설정
     port = '/dev/ttyACM0',
     baudrate = 9600
 )
 
-dict_result = {}
-
-
 # load the object detection network
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 
-# create video sources & outputs
-input = jetson.utils.videoSource("/dev/video0") #/dev/video0
+# 영상의 sources & outputs 생성
+input = jetson.utils.videoSource("/dev/video0")
 output = jetson.utils.videoOutput()
 
-ser.write( str( 5 ).encode() )
-# process frames until the user exits
-while True:
+ser.write(str( 5 ).encode()) # 시리얼 통신
 
-    # capture the next image
+while True:
     img = input.Capture()
 
-    # detect objects in the image (with overlay)
+    # 감지된 objects를 리턴
     detections = net.Detect(img, overlay="box,labels,conf")
 
+    # 감지된 objects 결과를 아두이노로 전송할 message로 저장
     for detection in detections:
+        if detection.ClassID == 1: # Person ClassID = 1
+            msg[car_cnt] += 1
+            msg[car_area].append(detection.Area)
+        elif detection.ClassID == 3: # Car ClassID = 3
+            msg[person_cnt] += 1
+            msg[person_area].append(detection.Area)
 
-        if detection.ClassID == 1:
-            pCount += 1
-
-        elif detection.ClassID == 3:
-            pass
-
+    print( "인식된 사람의 수 :: ", msg[person_cnt] )
     
-    # output.Render(img)
-
-    # net.PrintProfilerTimes()
-
-    print( "인식된 사람의 수 :: ", pCount )
-    ser.write( str( 50 - pCount * 20 ).encode() )
-    pCount = 0
-    ser.write( str( 0 ).encode() )
+    # 시연 예시)
+    # 속도 제어 : 50 - 사람 수 * 20
+    ser.write(str( 50 - msg[person_cnt] * 20 ).encode()) # 시리얼 통신
 
     # exit on input/output EOS
     if not input.IsStreaming() or not output.IsStreaming():
         break
-
-
-
